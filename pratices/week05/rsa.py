5da6a2480a254d7e5fd7f592f93d7dc034ea59af
1801040111 <1801040111@s.hanu.edu.vn>
2020-10-13 15:25:36 +0700

    clean code up

--- pratices/week05/rsa.py
+++ pratices/week05/rsa.py
@@ -1,24 +1,22 @@
 def solveEquation():
-    a = 667
-    b = 937
-    c = 2537
-    binary = bin(b)
-    listResult = []
+    base = 667
+    exp = 937
+    modular = 2537
+    binary = bin(exp)
     multiply = 1
-    print(binary)
     for digit in range(len(binary) - 1, 1, -1):
         possibleExpo = len(binary) - 1 - digit
         if binary[digit] == '1':
             targetExp = possibleExpo
             powerOfTwo = pow(2, targetExp)
-            resultAfterMod = pow(a, powerOfTwo, c)
+            resultAfterMod = pow(base, powerOfTwo, modular)
 
             multiply *= resultAfterMod
 
-    finalResult = multiply % c
+    finalResult = multiply % modular
 
-    print(finalResult)
+    return finalResult
 def main():
-    solveEquation()
+    print(solveEquation())
 
 main()
